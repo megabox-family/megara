09b00d87cf767727493dbaa48cd8d15b6c6ba3e5
Jordan Cooper <jordan@cooperplanet.com>
2019-11-13 19:33:48 -0800

    Get bot running

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const run = () => {
-  console.log('Hello world')
-}
+const config = require('../config')
 
-run()
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`)
+})
+
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong')
+  }
+})
+
+bot.login(config.botToken)
