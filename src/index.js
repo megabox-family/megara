c60cc34c5f302b59a4cf7e45a94756d066c0f17f
Jordan Cooper <jordan@cooperplanet.com>
2020-05-11 15:40:19 -0700

    Add something to test something

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 
 bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`)
+  console.log(`Gosh! Bogged in as ${bot.user.tag}!`)
 })
 
 bot.on('message', message => {
