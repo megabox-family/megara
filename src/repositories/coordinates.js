ec0e9f96f8ceb94386ee4af1425a0faeb2f03ac0
Jordan Cooper <jordan@cooperplanet.com>
2020-09-30 23:53:44 -0700

    Sort and safety check coordinates list

--- src/repositories/coordinates.js
+++ src/repositories/coordinates.js
@@ -4,13 +4,13 @@ const { v4: uuid } = require('uuid')
 
 const getAllCoordinates = async () => {
   return await pgPool
-    .query(`select * from coordinates;`)
+    .query(`select * from coordinates order by name;`)
     .then(res => camelize(res.rows))
 }
 
 const getCoordinatesByOwner = async owner => {
   return await pgPool
-    .query(`select * from coordinates where owner = $1;`, [owner])
+    .query(`select * from coordinates where owner = $1 order by name;`, [owner])
     .then(res => camelize(res.rows))
 }
 
