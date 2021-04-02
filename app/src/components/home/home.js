2fe34fbe7277bdb773c434f71f75807e6610fa43
Jordan Cooper <jordan@cooperplanet.com>
2021-04-01 17:28:39 -0700

    Create coming soon page

--- app/src/components/home/home.js
+++ app/src/components/home/home.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import './home.scss'
+import megaboxGray from '../../media/megabox-gray.svg'
+import nitroStar from '../../media/nitro-star.svg'
 
 const Home = () => {
-  return <h1 className="hello">Hello World!</h1>
+  return (
+    <div className="coming-soon">
+      <div className="header-image">
+        <div className="header-content">
+          <img className="small-nitro-star" src={nitroStar} />
+          <h1 className="header-text">Megabox</h1>
+        </div>
+      </div>
+      <div className="content">
+        <span className="coming-soon-message">
+          🚧 Channel Manager Coming Soon 🚧
+        </span>
+        <span className="coming-soon-subtitle">Just you wait...</span>
+      </div>
+      <div className="footer">
+        <div className="footer-content">
+          <img className="footer-image" src={megaboxGray} />
+          <span className="footer-text">Megabox</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|svg|gif)$/i,
         use: [
           {
             loader: 'file-loader',
