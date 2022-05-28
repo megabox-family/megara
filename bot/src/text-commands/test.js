fb95e7e79a9b04898fa2629bc7a8e07c682ba831
root <root@ASexyDesktop.localdomain>
2022-05-28 05:19:57 -0600

    Made test function temporarily a way to generate notification opt-in / out buttons.

--- bot/src/text-commands/test.js
+++ bot/src/text-commands/test.js
@@ -1,28 +1,41 @@
-import { getBot } from '../cache-bot.js'
+import { MessageActionRow, MessageButton } from 'discord.js'
 import { getCommandName, adminCheck } from '../utils/text-commands.js'
+import { getAnnouncementChannel } from '../repositories/guilds.js'
 
 const command = getCommandName(import.meta.url)
 
 export default async function (message, commandSymbol, args) {
-  getBot().guilds.cache.forEach(guild => {
-    const serverNotificationSquad = guild.roles.cache.find(
-        role => role.name === `server notification squad`
-      ),
-      channelNotificationSquad = guild.roles.cache.find(
-        role => role.name === `channel notification squad`
-      ),
-      colorNotificationSquad = guild.roles.cache.find(
-        role => role.name === `color notification squad`
-      )
+  if (!(await adminCheck(message, commandSymbol, command))) return
 
-    guild.members.cache.forEach(member => {
-      // member => console.log(member.user.username)
+  const guild = message.guild,
+    announcementChannelId = await getAnnouncementChannel(guild.id),
+    announcementChannel = guild.channels.cache.get(announcementChannelId),
+    channelNotificationSquad = guild.roles.cache.find(
+      role => role.name === `channel notification squad`
+    ),
+    colorNotificationSquad = guild.roles.cache.find(
+      role => role.name === `color notification squad`
+    ),
+    channelButtonRow = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId(`!subscribe: ${channelNotificationSquad.id}`)
+        .setLabel(`Subscribe to channel notifications`)
+        .setStyle('PRIMARY'),
+      new MessageButton()
+        .setCustomId(`!unsubscribe: ${channelNotificationSquad.id}`)
+        .setLabel(`Unsubscribe from channel notifications`)
+        .setStyle('DANGER')
+    ),
+    colorButtonRow = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId(`!subscribe: ${colorNotificationSquad.id}`)
+        .setLabel(`Subscribe to color notifications`)
+        .setStyle('PRIMARY'),
+      new MessageButton()
+        .setCustomId(`!unsubscribe: ${colorNotificationSquad.id}`)
+        .setLabel(`Unsubscribe from color notifications`)
+        .setStyle('DANGER')
+    )
 
-      if (![`Omegara`, `megara`].includes(member.user.username)) {
-        member.roles.add(serverNotificationSquad.id)
-        member.roles.add(channelNotificationSquad.id)
-        member.roles.add(colorNotificationSquad.id)
-      }
-    })
-  })
+  announcementChannel.send({ components: [channelButtonRow, colorButtonRow] })
 }
