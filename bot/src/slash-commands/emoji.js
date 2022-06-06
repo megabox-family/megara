044a76b34c9bc7e310a9cba70e1280cd904061e3
root <root@ASexyDesktop.localdomain>
2022-06-06 17:35:02 -0600

    pprn

--- bot/src/slash-commands/emoji.js
+++ bot/src/slash-commands/emoji.js
@@ -222,10 +222,10 @@ export default function (interaction) {
       interaction.reply(`🚔`)
       break
     case `🚓`:
-      interaction.reply(`<a:peperun:983505269730467901>`)
+      interaction.reply(`<a:peperun:983514136946311218>`)
       break
     case `🚔`:
-      interaction.reply(`<a:peperun:983505269730467901>`)
+      interaction.reply(`<a:peperun:983514136946311218>`)
       break
     default:
       const emojis = [
