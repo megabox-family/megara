7403d46b97a80860173a92dc23e47f93f7c919f7
ninbura <gabriel@ninbura.com>
2022-12-08 10:14:27 -0700

    Added the flip-a-coin slash command.

---
+++ bot/src/slash-commands/flip-a-coin.js
@@ -0,0 +1,33 @@
+import { ApplicationCommandOptionType } from 'discord.js'
+
+export const description = `Flips a coin and displays the result`
+export const dmPermission = false,
+  defaultMemberPermissions = `0`,
+  options = [
+    {
+      name: `make-private`,
+      description: `If true, only you will see the result. If false or left blank, others can see the result.`,
+      type: ApplicationCommandOptionType.Boolean,
+      required: false,
+    },
+  ]
+
+export default async function (interaction) {
+  const options = interaction.options
+
+  let makePrivate = options.getBoolean(`make-private`)
+
+  makePrivate = makePrivate === null ? false : true
+
+  await interaction.deferReply({ ephemeral: makePrivate })
+
+  const headsOrTails = Math.round(Math.random()) === 1 ? `heads` : `tails`
+
+  await interaction.editReply(`
+    🪙\
+
+    \n👍 🫴 🫳\
+
+    \nThe coin landed on **${headsOrTails}**.
+  `)
+}
