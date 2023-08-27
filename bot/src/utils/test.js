9af51c1ee7f09b6b194954a0d73976531b8693b2
ninbura <gabriel@ninbura.com>
2023-08-27 03:52:53 -0600

    fixed issue where chat wasn't disabled even when specified via the /create-voice-channel command

--- bot/src/utils/test.js
+++ bot/src/utils/test.js
@@ -1,9 +1,8 @@
 import { getAlphaChannelType } from './channels.js'
 
 export default async function (bot) {
-  const guild = bot.guilds.cache.get(`711043006253367426`),
-    channel = guild.channels.cache.get(`1123430789300895886`),
-    alphaChannelType = getAlphaChannelType(channel)
-
+  // const guild = bot.guilds.cache.get(`711043006253367426`),
+  //   channel = guild.channels.cache.get(`1123430789300895886`),
+  //   alphaChannelType = getAlphaChannelType(channel)
   // console.log(channel?.name, alphaChannelType)
 }
