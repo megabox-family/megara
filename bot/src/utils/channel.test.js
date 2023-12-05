936667163f100254ead5c160b9fca8f256d95dc3
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2023-12-05 06:45:00 -0500

    still not working, but a different error now

--- bot/src/utils/channel.test.js
+++ bot/src/utils/channel.test.js
@@ -1,15 +1,19 @@
+// import { jest } from '@jest/globals'
 import { emptyChannelSortingQueue } from './channels.js'
+jest.useFakeTimers()
 // import * as channel from './channels.js'
 
 describe('channel sorting queue', () => {
   // need fake channel sorting queue, need to be able to figure out what a Collection looks like.
-  test('emptyChannelSortingQueue', () => {
+  test('emptyChannelSortingQueue', async () => {
     const channelSortingQueue = [{ guildId: 1, bypassComparison: true }]
-    emptyChannelSortingQueue(channelSortingQueue)
+    await emptyChannelSortingQueue(channelSortingQueue)
     expect(channelSortingQueue).toEqual({})
     // maybe need to do channel.channelSortingQueue equals empty?
   })
 })
+
+afterEach(cleanup)
 
 // async function emptyChannelSortingQueue() {
 //   if (channelSortingQueue.size === 0) return
